db66c41efaec9ec297c485d66da910c956264e9a
Ricardo Meléndez <ricardomelendezao@gmail.com>
2023-02-10 13:22:39 -0500

    testing the api

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
