7deda8d2aa75c3071abc414e9592e9aa343307b1
Ricardo Meléndez <ricardomelendezao@gmail.com>
2023-02-09 23:37:13 -0500

    setup the table booking api

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics, viewsets
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.RetrieveDestroyAPIView):
+    # The queryset attribute is not filtered, but the
+    # DRF automatically filters it based on the pk
+    # passed in the URL.
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    
